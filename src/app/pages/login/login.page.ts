ff71a8783df226b16535b977cc0e2d7ac10ff298
clodioneto <clodio.alves@gmail.com>
2021-10-27 15:02:58 -0300

    Tela Login/Cadastro

---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  @ViewChild(IonSlides) slides: IonSlides
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  segmentChanged(event: any) {
+    if (event.detail.value === 'login'){
+      this.slides.slidePrev();
+
+    }else {
+      this.slides.slideNext();
+    }
+    
+  }
+
+}
